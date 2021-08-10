a1a1a30c93b8bd05d8aa0848d0825c943979ba3a
PanQL <panqinglin00@163.com>
2021-08-10 23:55:39 +0800

    complete seq

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,14 +1,12 @@
 #![feature(extend_one)]
-use proc_macro::{TokenStream};
-use proc_macro2::{TokenTree, Group, Literal};
-use syn::{Token, parse_macro_input, Ident, LitInt};
+use proc_macro::TokenStream;
+use proc_macro2::{Delimiter, Group, Literal, TokenTree};
 use syn::parse::{Parse, ParseStream, Result};
+use syn::{parse_macro_input, Ident, LitInt, Token};
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    eprintln!("{:?}", input);
     let mut seq_generator = parse_macro_input!(input as SeqGenerator);
-    //eprintln!("{:#?}", seq_generator);
 
     match seq_generator.generate() {
         Ok(stream) => stream.into(),
@@ -17,66 +15,174 @@ pub fn seq(input: TokenStream) -> TokenStream {
 }
 
 #[derive(Debug)]
-struct SeqGenerator{
+struct SeqGenerator {
     replacement: Ident,
-    range_left: LitInt,
-    range_right: LitInt,
-    template: proc_macro2::Group,
-    result: proc_macro2::TokenStream,
+    left: u64,
+    right: u64,
+    template: Group,
 }
 
 impl Parse for SeqGenerator {
     fn parse(input: ParseStream) -> Result<Self> {
         let replacement: Ident = input.parse()?;
         input.parse::<Token![in]>()?;
-        let range_left: LitInt = input.parse()?;
+        let left = input.parse::<LitInt>()?.base10_parse::<u64>()?;
         input.parse::<Token![.]>()?;
         input.parse::<Token![.]>()?;
-        let range_right: LitInt = input.parse()?;
-        let group: proc_macro2::Group = input.parse()?;
+        let right = if let Ok(_) = input.parse::<Token![=]>() {
+            input.parse::<LitInt>()?.base10_parse::<u64>()? + 1
+        } else {
+            input.parse::<LitInt>()?.base10_parse::<u64>()?
+        };
+        let group: Group = input.parse()?;
         Ok(SeqGenerator {
             replacement,
-            range_left,
-            range_right,
+            left,
+            right,
             template: group.clone(),
-            result: proc_macro2::TokenStream::new(),
         })
     }
 }
 
 impl SeqGenerator {
     fn generate(&mut self) -> Result<proc_macro2::TokenStream> {
-        let left  = self.range_left.base10_parse::<u64>()?;
-        let right  = self.range_right.base10_parse::<u64>()?;
-
-        for number in left..right {
-            self.result.extend(self.apply_group(&self.template, number).stream());
+        let mut result = proc_macro2::TokenStream::new();
+        let middle = self.expand_hashtag_parentheses(&self.template);
+        if self.check_group_iteration(&middle) {
+            let stream_template = middle.stream().into_iter().collect();
+            for num in self.left..self.right {
+                result.extend(self.apply_token_stream(&stream_template, num));
+            }
+        } else {
+            result.extend(middle.stream());
         }
-        //eprintln!("{:?}", self.result);
-        Ok(self.result.clone())
+        Ok(result)
     }
 
-    fn apply_group(&self, group: &proc_macro2::Group, current_num: u64) -> Group {
-        let mut new_stream = proc_macro2::TokenStream::new();
+    fn expand_hashtag_parentheses(&self, group: &Group) -> Group {
         let delimiter = group.delimiter();
-        group.stream().into_iter().for_each(|tt|{
+        let mut new_stream: Vec<TokenTree> = Vec::new();
+        let mut stream_iter = group.stream().into_iter();
+        loop {
+            let tt = match stream_iter.next() {
+                Some(tt) => tt,
+                None => break,
+            };
             match tt {
+                TokenTree::Group(sub_group) => {
+                    let new_sub_group = self.expand_hashtag_parentheses(&sub_group);
+                    new_stream.push(TokenTree::Group(new_sub_group));
+                }
+                TokenTree::Punct(punct) => {
+                    let punct_backup = TokenTree::Punct(punct.clone());
+                    match punct.as_char() {
+                        '#' => {
+                            let middle = stream_iter.next().unwrap();
+                            match middle {
+                                TokenTree::Group(sub_group) => {
+                                    if sub_group.delimiter() == Delimiter::Parenthesis {
+                                        let _ = stream_iter.next();
+                                        let sub_template = sub_group.stream().into_iter().collect();
+                                        for num in self.left..self.right {
+                                            new_stream.append(
+                                                &mut self.apply_token_stream(&sub_template, num),
+                                            );
+                                        }
+                                    } else {
+                                        new_stream.push(punct_backup);
+                                        new_stream.push(TokenTree::Group(sub_group));
+                                    }
+                                }
+                                token => {
+                                    new_stream.push(punct_backup);
+                                    new_stream.push(token.clone());
+                                }
+                            }
+                        }
+                        _ => new_stream.push(punct_backup),
+                    }
+                }
+                token => new_stream.push(token),
+            }
+        }
+        let mut result = proc_macro2::TokenStream::new();
+        result.extend(new_stream);
+        Group::new(delimiter, result)
+    }
+
+    fn apply_token_stream(&self, stream: &Vec<TokenTree>, num: u64) -> Vec<TokenTree> {
+        let mut new_stream: Vec<TokenTree> = Vec::new();
+        let mut stream_iter = stream.iter();
+        loop {
+            let tt = match stream_iter.next() {
+                Some(tt) => tt,
+                None => break,
+            };
+            match tt {
+                TokenTree::Group(sub_group) => {
+                    let sub_stream = sub_group.stream().into_iter().collect();
+                    let new_sub_stream = self.apply_token_stream(&sub_stream, num);
+                    let mut new_token_stream = proc_macro2::TokenStream::new();
+                    new_token_stream.extend(new_sub_stream);
+                    let new_sub_group = Group::new(sub_group.delimiter(), new_token_stream);
+                    new_stream.push(TokenTree::Group(new_sub_group));
+                }
+                TokenTree::Punct(punct) => match punct.as_char() {
+                    '#' => {
+                        let middle = stream_iter.next().unwrap();
+                        match middle {
+                            TokenTree::Ident(ident) => {
+                                let last_token = new_stream.pop();
+                                match last_token {
+                                    Some(TokenTree::Ident(last_ident)) => {
+                                        if ident.eq(&self.replacement) {
+                                            let new_name =
+                                                format!("{}{}", last_ident.to_string(), num);
+                                            let new_ident =
+                                                Ident::new(&new_name, last_ident.span());
+                                            new_stream.push(TokenTree::Ident(new_ident));
+                                        } else {
+                                            new_stream.push(TokenTree::Ident(last_ident));
+                                            new_stream.push(TokenTree::Ident(ident.clone()));
+                                        }
+                                    }
+                                    token => {
+                                        if let Some(last) = token {
+                                            new_stream.push(last);
+                                        }
+                                        if ident.eq(&self.replacement) {
+                                            let literal = Literal::u64_unsuffixed(num);
+                                            new_stream.push(TokenTree::Literal(literal));
+                                        } else {
+                                            new_stream.push(TokenTree::Ident(ident.clone()));
+                                        }
+                                    }
+                                }
+                            }
+                            token => new_stream.push(token.clone()),
+                        }
+                    }
+                    _ => new_stream.push(TokenTree::Punct(punct.clone())),
+                },
                 TokenTree::Ident(ident) => {
                     if ident.eq(&self.replacement) {
-                        let literal = Literal::u64_unsuffixed(current_num);
-                        //let new_ident = proc_macro2::Ident::new(&format!("{}", current_num), ident.span());
-                        new_stream.extend_one(TokenTree::Literal(literal));
+                        let literal = Literal::u64_unsuffixed(num);
+                        new_stream.push(TokenTree::Literal(literal));
                     } else {
-                        new_stream.extend_one(TokenTree::Ident(ident.clone()));
+                        new_stream.push(TokenTree::Ident(ident.clone()));
                     }
-                },
-                TokenTree::Group(sub_group) => {
-                    let new_sub_group = self.apply_group(&sub_group, current_num);
-                    new_stream.extend_one(TokenTree::Group(new_sub_group));
-                },
-                token => new_stream.extend_one(token),
+                }
+                token => new_stream.push(token.clone()),
             }
-        });
-        proc_macro2::Group::new(delimiter, new_stream)
+        }
+        new_stream
+    }
+
+    fn check_group_iteration(&self, stream: &Group) -> bool {
+        stream.stream().into_iter().any(|token| match token {
+            TokenTree::Ident(ident) => ident.eq(&self.replacement),
+            TokenTree::Group(group) => self.check_group_iteration(&group),
+            _ => false,
+        })
     }
 }
